01750000f9c0e701ffa7bb60075c8c6dfe7e3dc6
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-22 13:03:48 -0300

    adicionado métodos abstratod de atacar e defender

--- Personagem.ts
+++ Personagem.ts
@@ -1,6 +1,8 @@
 import { Util } from "./Util";
 
-export class Personagem {
+//métodos abstratos só podem aparecer em classes abstratas
+//uma classe abstrata não pode ser instanciada
+export abstract class Personagem {
   protected _nome: string;
   protected _armadura: number;
   protected _vidaAtual: number;
@@ -12,4 +14,9 @@ export class Personagem {
     this._vidaMaxima = Util.randomizar(100, 1_000);
     this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
   }
+
+  //métodos abstratos possuem uma assinatura sem ter uma implementação
+  public abstract atacar(): string;
+
+  public abstract defender(atacante: Personagem): number;
 }
