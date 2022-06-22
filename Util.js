a9d8166720ab853794857c9fd63a2c225fd0094e
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-22 10:09:55 -0300

    configuração do typescript para remoção de comentário dos arquivos compilados

--- Util.js
+++ Util.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Util = void 0;
 class Util {
-    //um método estático é um método que não precisa da instancia da classe para ser invocado.
     static randomizar(inicio, fim) {
         return inicio + Math.random() * (fim - inicio);
     }
