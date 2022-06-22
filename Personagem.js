976c32e83fd5311c93e631cbcd43bb8805ae4d45
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-22 12:31:27 -0300

    passando valores padrões para os atributos, caso não sejam instanciados

--- Personagem.js
+++ Personagem.js
@@ -5,8 +5,8 @@ const Util_1 = require("./Util");
 class Personagem {
     constructor(nome) {
         this._nome = nome;
-        this._armadura = Util_1.Util.randomizar(1000, 10000);
-        this._vidaMaxima = Util_1.Util.randomizar(200, 10000);
+        this._armadura = Util_1.Util.randomizar(100, 1000);
+        this._vidaMaxima = Util_1.Util.randomizar(100, 1000);
         this._vidaAtual = Util_1.Util.randomizar(40, this._vidaMaxima);
     }
 }
--- Personagem.ts
+++ Personagem.ts
@@ -8,8 +8,8 @@ export class Personagem {
 
   constructor(nome: string) {
     this._nome = nome;
-    this._armadura = Util.randomizar(1_000, 10_000);
-    this._vidaMaxima = Util.randomizar(200, 10_000);
+    this._armadura = Util.randomizar(100, 1_000);
+    this._vidaMaxima = Util.randomizar(100, 1_000);
     this._vidaAtual = Util.randomizar(40, this._vidaMaxima);
   }
 }
